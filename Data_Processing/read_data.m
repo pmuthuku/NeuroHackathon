65b917f06c78a7e4b95bdb57b5b57b4d7d56e71c
Prasanna Kumar Muthukumar <pmuthuku@cs.cmu.edu>
2016-05-25 11:34:29 -0400

    Created data files the way Waleed wanted me to.
    Also fixed a bug in the code that would have replicated frames.

--- Data_Processing/read_data.m
+++ Data_Processing/read_data.m
@@ -12,14 +12,18 @@ for i = 1:size(PV_spiketimes.M, 2)
     
     if(~isnan(PV_spiketimes.M(1,i))) %NaNs denote boundaries
         frames = get_PV_windowed_frames(PV_spiketimes.M(:,i));
+    
+        if size(frames, 1) > 0
+            filenm = ['training_data/PV_' num2str(PV_spiketimes.M(1,i)) '.mat'];
+            save(filenm, 'frames');
+        
+            PV_frames = [PV_frames; frames];
+        end
     end
     
-    PV_frames = [PV_frames; frames];
     
 end
 
-
-% Remember to do something about the empty frames
 
 %% Pyr data
 
@@ -29,12 +33,17 @@ Pyr_frames = [];
 
 % Each column of Pyr_spiketimes.M is an upstate
 for i = 1:size(Pyr_spiketimes.M, 2)
-   
-    if(~isnan(Pyr_spiketimes.M(1,i))) 
-       frames = get_Pyr_windowed_frames(Pyr_spiketimes.M(:,i)); 
-    end
     
-    Pyr_frames = [Pyr_frames; frames];
+    if(~isnan(Pyr_spiketimes.M(1,i)))
+        frames = get_Pyr_windowed_frames(Pyr_spiketimes.M(:,i));
+        
+        if size(frames, 1) > 0
+            filenm = ['training_data/Pyr_' num2str(Pyr_spiketimes.M(1,i)) '.mat'];
+            save(filenm, 'frames');
+            
+            Pyr_frames = [Pyr_frames; frames];  
+        end
+    end
     
 end
 
@@ -48,9 +57,14 @@ for i = 1:size(SST_spiketimes.M, 2)
     
     if(~isnan(SST_spiketimes.M(1,i)))
         frames = get_SST_windowed_frames(SST_spiketimes.M(:,i));
+        
+        if size(frames, 1) > 0
+           filenm = ['training_data/SST_' num2str(SST_spiketimes.M(1,i)) '.mat'];
+           save(filenm, 'frames');
+           
+           SST_frames = [SST_frames; frames];
+        end
     end
-    
-    SST_frames = [SST_frames; frames];
     
 end
 
